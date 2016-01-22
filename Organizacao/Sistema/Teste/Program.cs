074a27390039078bb98c91f98d92e638fa4c78c1
Ale DP <dpsantos@gmail.com>
2016-01-22 09:19:34 -0200

    Oranizacao

---
+++ Organizacao/Sistema/Teste/Program.cs
@@ -0,0 +1,19 @@
+﻿using Organizacao.Sistema.Conta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizacao.Sistemas.Teste
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Contas c = new Contas();
+			c.Deposita(1000);
+			Console.WriteLine(c.Saldo);
+		}
+	}
+}
